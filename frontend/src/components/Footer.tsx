8fbf698dcbd47eeefbb8af9f9b11899dff71d967
marceloacosta <acosta.cavalero@gmail.com>
2024-02-06 12:15:46 -0300

    update footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer: React.FC = () => {
         <div className=" flex flex-row justify-between py-3 text-sm">
           <div className="inline-flex items-center">
             <CloudIcon className="w-5 h-5 mr-1.5" />
-            Powered by Amazon Web Services
+            Powered by ZirconTech
           </div>
           <div className="inline-flex items-center hover:underline underline-offset-2">
             <img
